4b66759debf69fb2d7ca35da73d60a613cc3d17f
ksdyb.park <ksdyb@amazon.com>
2023-02-04 12:05:32 +0900

    update

--- stable-diffusion/inference-test.py
+++ stable-diffusion/inference-test.py
@@ -5,9 +5,10 @@ from inference import handler
 logger = getLogger()
 logger.setLevel(INFO)
 
-def StableDiffusion(txt_data):
+def StableDiffusion(txt_data, fname):
     event = {
-        'body': txt_data
+        'body': txt_data,
+        'fname': fname
     }
 
     try:
@@ -18,11 +19,14 @@ def StableDiffusion(txt_data):
         
 def main():
     prompt = 'a photo of an astronaut riding a horse on mars'
+    fname =  'astronaut_rides_horse.png'
     logger.debug('text: %s', prompt)
+
     print("text: "+ prompt)
 
-    result = StableDiffusion(prompt)
+    result = StableDiffusion(prompt, fname)
     logger.debug('url: %s', result['body'])
+    
     print("url: "+ result['body'])
         
 if __name__ == '__main__':
