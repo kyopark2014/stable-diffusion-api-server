87f9e2454a7886af8507568d0d25f05f7c0b49fb
ksdyb.park <ksdyb@amazon.com>
2023-02-04 14:14:15 +0900

    update

--- stable-diffusion/inference.py
+++ stable-diffusion/inference.py
@@ -14,7 +14,12 @@ logger.debug('device: %s', device)
 
 def ImageGenerator(prompt):
     model_id = "runwayml/stable-diffusion-v1-5"
-    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+
+    if device == "cuda":
+        pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    else:
+        pipe = StableDiffusionPipeline.from_pretrained(model_id)
+
     pipe = pipe.to(device)
 
     # prompt = "a photo of an astronaut riding a horse on mars"    
