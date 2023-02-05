8f635fe2a96076e4c56c65c1448052392ebaa089
ksdyb.park <ksdyb@amazon.com>
2023-02-05 22:03:35 +0900

    update

--- cdk-stable-diffusion/lib/cdk-stable-diffusion-stack.ts
+++ cdk-stable-diffusion/lib/cdk-stable-diffusion-stack.ts
@@ -1,16 +1,137 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from "path";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as apiGateway from "aws-cdk-lib/aws-apigateway";
+import * as logs from "aws-cdk-lib/aws-logs";
 
 export class CdkStableDiffusionStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const stage = "dev";
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStableDiffusionQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create Lambda for stable diffusion
+    const mlLambda = new lambda.DockerImageFunction(this, "lambda-api", {
+      description: 'lambda function for stable diffusion',
+      functionName: 'lambda-stable-diffusion',
+      memorySize: 512,
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../text2image')),
+      timeout: cdk.Duration.seconds(300),
+    }); 
+
+    // version
+    const version = mlLambda.currentVersion;
+    const alias = new lambda.Alias(this, 'LambdaAlias', {
+      aliasName: stage,
+      version,
+    });
+
+    // api Gateway
+    const logGroup = new logs.LogGroup(this, 'AccessLogs', {
+      retention: 90, // Keep logs for 90 days
+    });
+    logGroup.grantWrite(new iam.ServicePrincipal('apigateway.amazonaws.com')); 
+
+    const api = new apiGateway.RestApi(this, 'image-classification-api-server', {
+      description: 'API Gateway for image classification',
+      endpointTypes: [apiGateway.EndpointType.REGIONAL],
+      binaryMediaTypes: ['*/*'],
+      deployOptions: {
+        stageName: stage,
+        accessLogDestination: new apiGateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apiGateway.AccessLogFormat.jsonWithStandardFields({
+          caller: false,
+          httpMethod: true,
+          ip: true,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          user: true
+        }),
+      },
+      // proxy: false
+    });   
+
+    mlLambda.grantInvoke(new iam.ServicePrincipal('apigateway.amazonaws.com'));
+
+    const templateString: string = `##  See http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
+    ##  This template will pass through all parameters including path, querystring, header, stage variables, and context through to the integration endpoint via the body/payload
+    #set($allParams = $input.params())
+    {
+    "body-json" : $input.json('$'),
+    "params" : {
+    #foreach($type in $allParams.keySet())
+        #set($params = $allParams.get($type))
+    "$type" : {
+        #foreach($paramName in $params.keySet())
+        "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
+            #if($foreach.hasNext),#end
+        #end
+    }
+        #if($foreach.hasNext),#end
+    #end
+    },
+    "stage-variables" : {
+    #foreach($key in $stageVariables.keySet())
+    "$key" : "$util.escapeJavaScript($stageVariables.get($key))"
+        #if($foreach.hasNext),#end
+    #end
+    },
+    "context" : {
+        "account-id" : "$context.identity.accountId",
+        "api-id" : "$context.apiId",
+        "api-key" : "$context.identity.apiKey",
+        "authorizer-principal-id" : "$context.authorizer.principalId",
+        "caller" : "$context.identity.caller",
+        "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
+        "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
+        "cognito-identity-id" : "$context.identity.cognitoIdentityId",
+        "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
+        "http-method" : "$context.httpMethod",
+        "stage" : "$context.stage",
+        "source-ip" : "$context.identity.sourceIp",
+        "user" : "$context.identity.user",
+        "user-agent" : "$context.identity.userAgent",
+        "user-arn" : "$context.identity.userArn",
+        "request-id" : "$context.requestId",
+        "resource-id" : "$context.resourceId",
+        "resource-path" : "$context.resourcePath"
+        }
+    }`    
+    const requestTemplates = { // path through
+      "image/jpeg": templateString,
+      "image/jpg": templateString,
+      "application/octet-stream": templateString,
+      "image/png" : templateString
+    }
+    
+    const upload = api.root.addResource('classifier');
+    upload.addMethod('POST', new apiGateway.LambdaIntegration(mlLambda, {
+      // PassthroughBehavior: apiGateway.PassthroughBehavior.NEVER,
+      passthroughBehavior: apiGateway.PassthroughBehavior.WHEN_NO_TEMPLATES,
+      requestTemplates: requestTemplates,
+      integrationResponses: [{
+        statusCode: '200',
+      }], 
+      proxy:false, 
+    }), {
+      methodResponses: [   // API Gateway sends to the client that called a method.
+        {
+          statusCode: '200',
+          responseModels: {
+            'application/json': apiGateway.Model.EMPTY_MODEL,
+          }, 
+        }
+      ]
+    }); 
+
+    new cdk.CfnOutput(this, 'apiUrl', {
+      value: api.url,
+      description: 'The url of API Gateway',
+    });
   }
 }
