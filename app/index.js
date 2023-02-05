7478f6234ae48fc42ea9388ab47f2b8f250fdd36
ksdyb.park <ksdyb@amazon.com>
2023-02-05 16:21:00 +0900

    update

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { PythonShell } = require("python-shell");
 const app = express();
-const port = 8080;
+const port = 8000;
 
 app.use(express.json());
 //app.use(express.static(path.join(__dirname, "model")));
