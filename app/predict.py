c9c3ea68a7e9858bbb8f1891da49fb80da58bebb
ksdyb.park <ksdyb@amazon.com>
2023-02-05 21:12:39 +0900

    update

--- app/predict.py
+++ app/predict.py
@@ -1,5 +1,12 @@
+from logging import INFO, DEBUG, StreamHandler, getLogger
+
+logger = getLogger()
+logger.setLevel(INFO)
+
 def main():
     print("python commend")
+    #logger.debug('event: %s', event)
+    logger.debug('log from the python')
 
     return "Ok"
 
--- app/server.js
+++ app/server.js
@@ -32,58 +32,7 @@ server.get('/text', (req,res) => {
         console.log(results);
     });
 
-    logger.debug('result:'+result);		
+    logger.debug('result:'+JSON.stringify(result));		
 
     res.send('request was received.');
 });
-
-//const app = express();
-//const port = 8000;
-
-//const logger = require('./logger'); 
-
-// const express = require('express');
-// const server = express();
-
-
-//const { PythonShell } = require("python-shell");
-//app.use(express.json());
-//app.use(express.static(path.join(__dirname, "model")));
-
-
-/*
-server.get('/text', (req,res) => {
-	text = req.header('text');
-		
-    console.log(text);
-
-    res.send('request was received.');
-});
-
-server.listen(8000, () => {
-	console.log("Start server...");
-}); */
-
-/*
-app.get("/get", (req, res) => {
-    text = req.header('text');
-    console.log(text);
-
-    var options = {
-        mode: "json",
-        pythonPath: "",
-        pythonOptions: ["-u"],
-        scriptPath: "",
-        args: [input],
-    };
-
-    PythonShell.run("predict.py", options, function(err, results) {
-        if(err) throw err;
-        
-        console.log(results);
-    });
-});
-
-app.listen(port, ()=>{
-    console.log(`app listening at http://localhost:$(port})`);
-}); */
