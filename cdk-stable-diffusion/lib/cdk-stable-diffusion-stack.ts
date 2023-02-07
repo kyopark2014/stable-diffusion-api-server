e367d8945e72f90774c3f52318ec7894066c9ed5
ksdyb.park <ksdyb@amazon.com>
2023-02-07 19:51:53 +0900

    update

--- cdk-stable-diffusion/lib/cdk-stable-diffusion-stack.ts
+++ cdk-stable-diffusion/lib/cdk-stable-diffusion-stack.ts
@@ -13,7 +13,7 @@ export class CdkStableDiffusionStack extends cdk.Stack {
     super(scope, id, props);
 
     // s3 deployment
-    const s3Bucket = new s3.Bucket(this, "gg-depolyment-storage",{
+  /*  const s3Bucket = new s3.Bucket(this, "gg-depolyment-storage",{
       // bucketName: bucketName,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
@@ -38,28 +38,17 @@ export class CdkStableDiffusionStack extends cdk.Stack {
     new s3Deploy.BucketDeployment(this, "UploadArtifact", {
       sources: [s3Deploy.Source.asset("../src")],
       destinationBucket: s3Bucket,
-    });
+    }); */
 
-    const stage = "dev";
-
-    // create lambda-funtional-url
-    const lambdaFunctionUrl = new lambda.Function(this, "LambdaFunctionUrl", {
-      description: 'lambda function url',
-      runtime: lambda.Runtime.NODEJS_14_X, 
-      code: lambda.Code.fromAsset("../lambda-function-url"), 
-      handler: "index.handler", 
-      timeout: cdk.Duration.seconds(3),
-      environment: {
-      }
-    }); 
+    const stage = "dev"; 
 
     // Create Lambda for stable diffusion
-    const mlLambda = new lambda.DockerImageFunction(this, "lambda-api", {
+    const mlLambda = new lambda.DockerImageFunction(this, "lambda-stable-diffusion", {
       description: 'lambda function for stable diffusion',
       functionName: 'lambda-stable-diffusion',
       memorySize: 512,
-      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../text2image')),
-      timeout: cdk.Duration.seconds(300),
+      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../../lambda')),
+      timeout: cdk.Duration.seconds(60),
     }); 
 
     // version
@@ -70,79 +59,8 @@ export class CdkStableDiffusionStack extends cdk.Stack {
     });
 
     // api Gateway
-    const logGroup = new logs.LogGroup(this, 'AccessLogs', {
-      retention: 90, // Keep logs for 90 days
-    });
-    logGroup.grantWrite(new iam.ServicePrincipal('apigateway.amazonaws.com')); 
+/*  mlLambda.grantInvoke(new iam.ServicePrincipal('apigateway.amazonaws.com'));
 
-    const api = new apiGateway.RestApi(this, 'stable-diffusion-api-server', {
-      description: 'API Gateway for Stable Diffusion',
-      endpointTypes: [apiGateway.EndpointType.REGIONAL],
-      binaryMediaTypes: ['*/*'],
-      deployOptions: {
-        stageName: stage,
-        accessLogDestination: new apiGateway.LogGroupLogDestination(logGroup),
-        accessLogFormat: apiGateway.AccessLogFormat.jsonWithStandardFields({
-          caller: false,
-          httpMethod: true,
-          ip: true,
-          protocol: true,
-          requestTime: true,
-          resourcePath: true,
-          responseLength: true,
-          status: true,
-          user: true
-        }),
-      },
-      // proxy: false
-    });   
-
-    mlLambda.grantInvoke(new iam.ServicePrincipal('apigateway.amazonaws.com'));
-
-    const templateString: string = `##  See http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-mapping-template-reference.html
-    ##  This template will pass through all parameters including path, querystring, header, stage variables, and context through to the integration endpoint via the body/payload
-    #set($allParams = $input.params())
-    {
-    "body-json" : $input.json('$'),
-    "params" : {
-    #foreach($type in $allParams.keySet())
-        #set($params = $allParams.get($type))
-    "$type" : {
-        #foreach($paramName in $params.keySet())
-        "$paramName" : "$util.escapeJavaScript($params.get($paramName))"
-            #if($foreach.hasNext),#end
-        #end
-    }
-        #if($foreach.hasNext),#end
-    #end
-    },
-    "stage-variables" : {
-    #foreach($key in $stageVariables.keySet())
-    "$key" : "$util.escapeJavaScript($stageVariables.get($key))"
-        #if($foreach.hasNext),#end
-    #end
-    },
-    "context" : {
-        "account-id" : "$context.identity.accountId",
-        "api-id" : "$context.apiId",
-        "api-key" : "$context.identity.apiKey",
-        "authorizer-principal-id" : "$context.authorizer.principalId",
-        "caller" : "$context.identity.caller",
-        "cognito-authentication-provider" : "$context.identity.cognitoAuthenticationProvider",
-        "cognito-authentication-type" : "$context.identity.cognitoAuthenticationType",
-        "cognito-identity-id" : "$context.identity.cognitoIdentityId",
-        "cognito-identity-pool-id" : "$context.identity.cognitoIdentityPoolId",
-        "http-method" : "$context.httpMethod",
-        "stage" : "$context.stage",
-        "source-ip" : "$context.identity.sourceIp",
-        "user" : "$context.identity.user",
-        "user-agent" : "$context.identity.userAgent",
-        "user-arn" : "$context.identity.userArn",
-        "request-id" : "$context.requestId",
-        "resource-id" : "$context.resourceId",
-        "resource-path" : "$context.resourcePath"
-        }
-    }`    
     const requestTemplates = { // path through
     //  "image/jpeg": templateString,
     //  "image/jpg": templateString,
@@ -174,6 +92,6 @@ export class CdkStableDiffusionStack extends cdk.Stack {
     new cdk.CfnOutput(this, 'apiUrl', {
       value: api.url,
       description: 'The url of API Gateway',
-    });
+    }); */
   }
 }
---
+++ lambda/lambda_function.py
@@ -0,0 +1,68 @@
+import json
+import boto3
+import io
+from PIL import Image
+import numpy as np
+
+s3 = boto3.client('s3')
+
+# import sagemaker
+# sess = sagemaker.Session()
+# mybucket = sess.default_bucket()        
+def parse_response(query_response):
+    """Parse response and return generated image and the prompt"""
+
+    response_dict = json.loads(query_response)
+    return response_dict["generated_image"], response_dict["prompt"]
+
+def handler(event, context):
+    #body = event['body']
+    print(event)
+    
+    #txt = body['text']
+
+    bucket = 'sagemaker-ap-northeast-2-677146750822'
+    
+    payload = {
+        "prompt": "astronaut on a horse",
+        "width": 768,
+        "height": 768,
+        "num_images_per_prompt": 1,
+        "num_inference_steps": 50,
+        "guidance_scale": 7.5,
+    }
+    
+    endpoint = 'jumpstart-example-infer-model-txt2img-s-2023-02-06-15-08-09-213'
+    runtime = boto3.Session().client('sagemaker-runtime')
+        
+    response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='application/x-text', Accept='application/json', Body=json.dumps(payload))
+    print(response)
+    
+    statusCode = response['ResponseMetadata']['HTTPStatusCode']
+    print('statusCode:', json.dumps(statusCode))
+    
+    if(statusCode==200):
+        response_payload = response['Body'].read().decode("utf-8")
+        generated_image, prompt = parse_response(response_payload)
+
+        print(prompt)
+        
+        img = Image.fromarray(np.uint8(generated_image))
+        img.save('c_pil.png')
+        
+        mybucket = bucket
+        mykey = 'output/filename.jpeg'
+
+        # translated image from numpy array
+        image = Image.fromarray(np.uint8(img))
+            
+        buffer = io.BytesIO()
+        image.save(buffer, "JPEG")
+        buffer.seek(0)
+            
+        s3.upload_fileobj(buffer, mybucket, mykey, ExtraArgs={ "ContentType": "image/jpeg"})
+        
+    return {
+        'statusCode': statusCode,
+        'body': json.dumps('Hello from Lambda!')
+    }
