19e2bce672841c236eff68b1358b65bf877feada
ksdyb.park <ksdyb@amazon.com>
2023-02-08 01:14:51 +0900

    update

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -71,15 +71,17 @@ def handler(event, context):
         print(prompt)
         
         #image = Image.fromarray(np.uint8(generated_image))
-        image = Image.fromqimage("L", (100,100), generated_image)
         #image = Image.frombuffer("L", (100,100), generated_image) (x)
         #image = Image.frombytes("L", (100,100), generated_image) (x)
+        #image = Image.fromqimage("L", (100,100), generated_image)
+        #image = Image.fromqpixmap
 
         #s3.upload_fileobj(image, mybucket, mykey, ExtraArgs={ "ContentType": "image/jpeg"})
             
-        buffer = io.BytesIO()
-        image.save(buffer, format="jpeg")
-        buffer.seek(0)
+        #buffer = io.BytesIO()
+        #image.save(buffer, format="jpeg")
+        #buffer.seek(0)
+        buffer = io.BytesIO(generated_image)
             
         s3.upload_fileobj(buffer, mybucket, mykey, ExtraArgs={ "ContentType": "image/jpeg"})
                     
